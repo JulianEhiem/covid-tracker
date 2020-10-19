3a79b85745ecaa638972699fd802ed59f5a36e2f
Julian Ehiem <jules.ehiem@gmail.com>
2020-10-19 06:35:47 -0400

    modified api call to return individual country data

--- src/App.js
+++ src/App.js
@@ -17,14 +17,24 @@ class App extends React.Component {
 
         this.setState({data: fetchedData})
     }
+
+    handleCountryChange = async (country) => {
+        const fetchedData = await fetchData(country);
+
+        this.setState({data: fetchData, country: country});
+
+
+    }
+
+
     render() {
-        const {data} = this.state;
+        const {data, country} = this.state;
 
         return (
             <div className={styles.container}>
                 <Cards  data={data} />
-                <CountryPicker />
-                <Chart />
+                <CountryPicker handleCountryChange={this.handleCountryChange}/>
+                <Chart data={data} country={country}/>
 
             </div>
         )
